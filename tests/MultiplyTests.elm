feafa525dcd2aa153e5e3f0cac9b82883fc99899
Emily Woods <emily@crate.io>
2018-03-04 16:45:59 +0100

    Add integration test

--- tests/MultiplyTests.elm
+++ tests/MultiplyTests.elm
@@ -3,6 +3,7 @@ module MultiplyTests exposing (..)
 import Expect exposing (Expectation)
 import Test exposing (..)
 import Multiply 
+import Number
 
 suite : Test
 suite =
@@ -15,6 +16,22 @@ suite =
             , test "It assigns a left and right floating point value" <|
                 \_ ->
                         Expect.equal { left = 2.0, right = 4.0 } (Multiply.multiply 2.0 4.0 )
+
+            ]
+            {-
+            Add integration tests with number e.g. 
+            -}
+        , describe "Multiplication is assigned with Numbers" 
+            [ test "It assigns a left and right value" <|
+                \_ ->
+                    let 
+                        x = 
+                            Number.number 2
+                        y = 
+                            Number.number 4
+                    in
+                        Expect.equal { left = { value = 2 }, right = { value = 4 } } ( Multiply.multiply x y ) 
+
 
             ]
         ]
